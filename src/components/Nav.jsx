9493398fda89b538ae65c2854465e3f013b45e5b
Md Javed Akhtar <78804616+jclbug@users.noreply.github.com>
2025-03-11 15:24:11 +0530

    site name changed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ export default function Nav({ onSetQuery, totalMovie }) {
     <nav className="flex justify-between max-w-[2200px] w-full ml-auto mr-auto bg-(--color-dark) items-center text-(--color-background-500) font-bold text-2xl pt-[16px] pb-[16px] pl-[48px] pr-[48px] ">
       <div className="flex items-center">
         <img src="logo3.svg" className="w-[48px]" />
-        <p className="mt-[18px] ml-[-15px]">Flimstr</p>
+        <p className="mt-[18px] ml-[-15px]">Flimstar</p>
       </div>
       <input
         onChange={(e) => onSetQuery(e.target.value)}
